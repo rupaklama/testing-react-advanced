3b173432d160bbfed07b143b6079f147a5d0c99a
pak <rupaklamadev@gmail.com>
2022-09-26 18:25:33 -0400

    initial commit

---
+++ src/__tests__/unit/components/case-1-Checkbox.test.tsx
@@ -0,0 +1,66 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import userEvent from '@testing-library/user-event'
+
+import { axe } from 'jest-axe'
+import { render, screen, fireEvent } from '@testing-library/react'
+
+import Checkbox from '../../../components/Checkbox'
+import { debug } from 'console'
+
+/**
+ * This checkbox component renders a checkbox with a label.
+ * Since we customized the default checkbox, we want to
+ * make sure it still works as a regular checkbox
+ * should.
+ */
+describe('The <Checkbox /> component', () => {
+  const defaultCheckboxProps = {
+    label: 'test_checkbox_label',
+    id: 'test_checkbox_id',
+    onChange: jest.fn(),
+    checked: false,
+  }
+
+  it('Should render the label and checkbox the user will see', () => {
+    // place to render this element
+    // const container = document.createElement('div')
+
+    // note - using ReactDOM for manual testing
+    // ReactDOM.render(<Checkbox {...defaultCheckboxProps} />, container)
+
+    // check to see if elements are present
+    // expect(container.querySelector('label')).not.toBeNull()
+    // expect(container.querySelector('input[type="checkbox"]')).not.toBeNull()
+    // console.log(container.innerHTML)
+
+    const { asFragment } = render(<Checkbox {...defaultCheckboxProps} />)
+
+    // to see rendered dom elements
+    screen.debug()
+
+    // NOTE - Returns a DocumentFragment of your rendered component.
+    // This can be useful if you need to avoid live bindings and see how your component reacts to events.
+    // Also, to Find all the DOM Elements on initial renders
+    // console.log(asFragment()) DocumentFragment {}
+
+    //  Snapshot Testing using 'asFragment()'
+    expect(asFragment()).toMatchSnapshot()
+
+    const inputElement = screen.getByRole('checkbox')
+    expect(inputElement).toBeInTheDocument()
+  })
+
+  it('Should make the checkbox accessible by setting the id and htmlFor attributes on label and checkbox', () => {
+    render(<Checkbox {...defaultCheckboxProps} />)
+
+    const labelElement = screen.getByLabelText('test_checkbox_label')
+    expect(labelElement).toBeInTheDocument()
+  })
+
+  it('Should call the onChange handler when it is provided', () => {})
+
+  it('Should change state correctly when clicked (checked and unchecked)', () => {})
+
+  it('should not fail any accessibility tests', async () => {})
+})
