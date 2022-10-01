9dfc2d2cf04152d518fa0f00e783fe73e5b1d4c7
pak <rupaklamadev@gmail.com>
2022-10-01 13:18:06 -0400

    context connected component tested

--- src/__tests__/unit/components/case-2-ProductTile.test.tsx
+++ src/__tests__/unit/components/case-2-ProductTile.test.tsx
@@ -1,18 +1,48 @@
 import React from 'react'
 import { axe } from 'jest-axe'
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import ProductTile from '../../../components/ProductTile'
 
 describe('The <ProductTile /> component', () => {
-  it('❌ renders a product tile with name, image and price', () => {
+  const defaultProps = {
+    id: 1,
+    name: 'test product name',
+    image: '/image.png',
+    price: 'from $12.99',
+    brand: 'adidas',
+    createdAt: '10-01-2022',
+    isActive: true,
+  }
 
+  it('renders a product tile with name, image and price', () => {
+    // type assertion to any so not to add all the additional default props
+    render(<ProductTile {...(defaultProps as any)} />)
+
+    const titleElement = screen.getByText(defaultProps.name)
+    const priceElement = screen.getByText(defaultProps.price)
+    const imageElement = screen.getByAltText(defaultProps.name)
+
+    expect(titleElement).toBeInTheDocument()
+    expect(priceElement).toBeInTheDocument()
+    expect(imageElement).toBeInTheDocument()
   })
 
-  it('❌ renders a product tile with name and price only', () => {
+  it('renders a product tile with name and price only when no image', () => {
+    // updating the default prop
+    render(<ProductTile {...({ ...defaultProps, image: undefined } as any)} />)
 
+    const imageElement = screen.queryByAltText(defaultProps.name)
+    // expect(imageElement).not.toBeInTheDocument()
+
+    // note - queryBy returns null if not found
+    expect(imageElement).toBeNull()
   })
 
-  it('❌ has no accessibility violations', async () => {
+  it('has no accessibility violations', async () => {
+    const { container } = render(<ProductTile {...(defaultProps as any)} />)
 
+    const results = await axe(container)
+    // console.log(results)
+    expect(results).toHaveNoViolations()
   })
 })
--- src/__tests__/unit/components/case-3-Header.test.tsx
+++ src/__tests__/unit/components/case-3-Header.test.tsx
@@ -1,11 +1,37 @@
 import React from 'react'
-import { render, fireEvent } from '@testing-library/react'
+import { render, screen, fireEvent } from '@testing-library/react'
 
 import Header from '../../../components/Header'
 import { FiltersContext } from '../../../context/filters'
+import userEvent from '@testing-library/user-event'
 
 describe('The Header component', () => {
-  it('❌ renders header correctly', () => {})
+  const defaultContext = {
+    toggleShowingFilters: jest.fn(),
+  }
 
-  it('❌ toggles the filter open when the Filter button is clicked', () => {})
+  // helper render function
+  function renderHeader(value = defaultContext) {
+    return render(
+      <FiltersContext.Provider value={...value as any}>
+        <Header />
+      </FiltersContext.Provider>,
+    )
+  }
+
+  it('renders header correctly', () => {
+    const { asFragment } = renderHeader()
+
+    // snapshot
+    expect(asFragment()).toMatchSnapshot()
+  })
+
+  it('toggles the filter open when the Filter button is clicked', () => {
+    renderHeader()
+
+    const filterButton = screen.getByRole('button', { name: /filter/i })
+    fireEvent.click(filterButton)
+
+    expect(defaultContext.toggleShowingFilters).toHaveBeenCalled()
+  })
 })
